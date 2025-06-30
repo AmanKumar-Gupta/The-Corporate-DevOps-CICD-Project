9d1d7a37344ea9c6f2bfd8cc282ed98911f23ced
Ubuntu <ubuntu@ip-172-31-35-243.ap-south-1.compute.internal>
2025-06-30 08:06:59 +0000

    Tried CI Pipeline

---
+++ Initial-VMs/sonar/sonarqube-instance.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "sonarqube" {
+  ami           = "ami-0f918f7e67a3323f0" # Replace with a valid Jenkins AMI
+  instance_type = "t2.small"
+  key_name      = "office-laptop"
+  security_groups = ["launch-wizard-1"]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt-get update -y
+    sudo apt install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker $USER
+    newgrp docker
+    docker run -d --name sonarqube -p 9000:9000 sonarqube:lts-community
+  EOF
+
+  tags = {
+    Name = "SonarQube Instance"
+  }
+}
